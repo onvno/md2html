f4fc5b9f783ac69887167414bb2568b7fd734554
onvno <leewei2020@126.com>
2016-09-20 13:38:02 +0800

    add:base convert package

---
+++ bin/mark.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+cdir=`pwd`
+
+prodName=(
+    "components"
+#    "kero"
+)
+docs="docs"
+
+# 创建out目录
+htmlDir="$cdir/out"
+if [ ! -d $htmlDir ]
+  then
+  mkdir $htmlDir
+
+fi
+for name in ${prodName[@]}
+do
+  # 创建out子目录
+  if [ ! -d "$htmlDir/$name" ]
+    then
+    mkdir "$htmlDir/$name"
+  fi
+
+  # md文档转html
+  cd "$docs/$name"
+  for fullName in `ls $1`
+  do
+    suffix="${fullName##*.}"
+    if [ "${suffix}"=="md" ]
+    then
+      prename="${fullName%.*}"
+      echo "$fullName"
+      marked "$fullName" -o "$htmlDir/$name/$prename.html"
+      if [ "$prename" = "SUMMARY" ]
+        then
+        echo "$htmlDir/$name/$prename.html"
+        sed -ig "s/\w*.md/$1.html/" $htmlDir/$name/$prename.html
+        rm $htmlDir/$name/$prename.htmlg
+      fi
+    fi   
+
+  done
+done
+
+# 替换SUMMARY.md链接
+# sed -i "s/查找字段/替换字段/g" `grep 查找字段 -rl 路径`
---
+++ hello.md
@@ -0,0 +1,17 @@
+<meta charset="UTF-8">
+
+
+
+# 大标题
+
+## 二级标题
+
+这是内容
+
+```bash
+node server
+```
+
+
+
+<h3>三级标题</h3>
